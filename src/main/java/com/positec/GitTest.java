0e2089976b165414ea4c22ff94a3db08b6be15db
JackChen <2846513836@qq.com>
2026-05-24 16:07:37 +0800

    second commit

--- src/main/java/com/positec/GitTest.java
+++ src/main/java/com/positec/GitTest.java
@@ -3,5 +3,7 @@ package com.positec;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+
     }
 }
